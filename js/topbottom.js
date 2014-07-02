0a2ba7bfd94f81c53e0431cfe8706181eea25f2d
shellphon <dont923@gmail.com>
2014-07-02 23:16:43 +0800

    change to use javascript

--- js/topbottom.js
+++ js/topbottom.js
@@ -1,22 +1,34 @@
 (function(){
 	var dontBody = document.body,
 		firstChildOfBody = dontBody.firstChild,
-		divTopTarget = document.createElement("div"),
-		divBottomTarget = document.createElement("div"),
+		// divTopTarget = document.createElement("div"),
+		// divBottomTarget = document.createElement("div"),
 		aTopD = document.createElement("a"),
 		aBottomD = document.createElement("a");
+	var bottomPos = document.body.offsetHeight;
 	//target指定id
-	divTopTarget.id="dontTop";
-	divBottomTarget.id="dontBottom";
+	// divTopTarget.id="dontTop";
+	// divBottomTarget.id="dontBottom";
 	//index target a
 	aTopD.id = "backTopDont";
 	aBottomD.id = "backBottomDont";
-	aTopD.href = "#dontTop";
-	aBottomD.href = "#dontBottom";
+	// aTopD.href = "#dontTop";
+	// aBottomD.href = "#dontBottom";
 
-	dontBody.insertBefore(divTopTarget,firstChildOfBody);
+	// dontBody.insertBefore(divTopTarget,firstChildOfBody);
 	dontBody.appendChild(aTopD);
 	dontBody.appendChild(aBottomD);
-	dontBody.appendChild(divBottomTarget);
-	dontBody.style.position = "relative";
+
+	aTopD.addEventListener("click",function(){
+		window.scrollTo(0, 0);
+	});
+	// if(bottomPos<10000){//改用window.scrollTo(x, y)来实现滚动
+	// }
+		bottomPos = 1000000;
+	aBottomD.addEventListener("click", function(){
+		console.log("debug:"+bottomPos);
+		window.scrollTo(0, bottomPos);
+	});
+	// dontBody.appendChild(divBottomTarget);
+	// dontBody.style.position = "relative";
 })();
